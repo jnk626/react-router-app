5c95c4d008eac8122d419f7a4c531f6a38598ee6
jnk626 <jnk626@pm.me>
2023-04-20 12:02:38 +0200

    first commit

---
+++ src/routes/root.jsx
@@ -0,0 +1,97 @@
+import { 
+    NavLink,
+    Outlet,
+    useLoaderData,
+    Form,
+    redirect,
+    useNavigation
+} from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
+import { useEffect } from "react";
+
+export async function action() {
+    const contact = await createContact();
+    return redirect(`/contacts/${contact.id}/edit`);
+}
+
+export async function loader({ request }) {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
+    return { contacts, q };
+}
+
+export default function Root() {
+    const { contacts, q } = useLoaderData();
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        document.getElementById("q").value = q;
+    }, [q]);
+    
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        <div>
+          <Form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+              defaultValue={q}
+            />
+            <div id="search-spinner" aria-hidden hidden="true"></div>
+            <div className="sr-only" aria-live="polite"></div>
+          </Form>
+        </div>
+        <nav>
+            {contacts.length ? (
+                <ul>
+                    {contacts.map((contact) => (
+                        <li key={contact.id}>
+                            <NavLink
+                            to={`contacts/${contact.id}`}
+                            className={({ isActive, isPending }) =>
+                                isActive
+                                    ? "active"
+                                    : isPending
+                                    ? "pending"
+                                    : ""
+                                }
+                            >
+                                {contact.first || contact.last ? (
+                                    <>
+                                        {contact.first} {contact.last}
+                                    </>
+                                ) : (
+                                    <i>No name</i>
+                                )}{" "}
+                                {contact.favorite && <span>*</span>}
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>
+                    <i>No contacts</i>
+                </p>
+            )}
+        </nav>
+        <Form method="post">
+            <button type="submit">New</button>
+        </Form>
+      </div>
+      <div 
+      id="detail"
+      className={
+        navigation.state === "loading" ? "loading" : ""
+      }
+      >
+        <Outlet />
+      </div>
+    </>
+  );
+}
